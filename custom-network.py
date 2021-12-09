bbe4251cc2679d3a62e24cb4bfde678c37d3d8ac
rupumped <rupumped@gmail.com>
2021-12-08 18:06:13 -0800

    Fixed illustration

--- custom-network.py
+++ custom-network.py
@@ -2,27 +2,37 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 
-torch.manual_seed(0)
+torch.manual_seed(3)
 
 # Make data
 N_DATA = 10000
-xx = 4*torch.rand(N_DATA,1)-2
-yy = torch.abs(xx)
+x = 4*torch.rand(N_DATA,1)-2
+y = torch.abs(x)
 
 # Initialize Model
 DIM_INPUT = 1
 DIM_OUTPUT = 1
 H = 2
-network = torch.nn.Sequential(
-    torch.nn.Linear(DIM_INPUT,H),
-    torch.nn.ReLU(),
-    torch.nn.Linear(H,DIM_OUTPUT)
-)
+
+class MyNet(torch.nn.Module):
+    def __init__(self) -> None:
+        super(MyNet, self).__init__()
+        self.m = torch.nn.Linear(1,2,bias=False)
+        self.relu = torch.nn.ReLU()
+
+    def forward(self, x):
+        s = self.m(x)
+        z = self.relu(s)
+        # breakpoint()
+        y = torch.sum(z,1)
+        return y
+
+network = MyNet()
 
 # Split dataset
-N_train = int(3*len(yy)/5)
-dataset = torch.utils.data.TensorDataset(xx, yy)
-train_dataset, val_dataset = torch.utils.data.dataset.random_split(dataset, [N_train,len(yy)-N_train])
+N_train = int(3*len(y)/5)
+dataset = torch.utils.data.TensorDataset(x, y)
+train_dataset, val_dataset = torch.utils.data.dataset.random_split(dataset, [N_train,len(y)-N_train])
 
 # Construct dataloaders for batch processing
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=32)
@@ -30,8 +40,8 @@ val_loader   = torch.utils.data.DataLoader(dataset=val_dataset  , batch_size=32)
 
 # Define learning hyperparameters
 loss_fn       = torch.nn.MSELoss(reduction='sum')
-learning_rate = .001
-n_epochs      = 200
+learning_rate = .00001
+n_epochs      = 2000
 optimizer     = torch.optim.Adam(network.parameters(), lr=learning_rate)
 
 # Initialize arrays for logging
@@ -82,7 +92,7 @@ axs[0].set_xlabel('Epoch')
 axs[0].set_ylabel('Loss')
 axs[0].legend()
 
-axs[1].plot(xx,yy,'.', label='Data')
+axs[1].plot(x,y,'.', label='Data')
 xs = torch.tensor([[-2],[-1],[0],[1],[2]]).type(torch.FloatTensor)
 ys = network(xs)
 axs[1].plot(xs.detach().numpy(),ys.detach().numpy(), label='Model')
